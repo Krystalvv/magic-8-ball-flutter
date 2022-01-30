9aaedd5fedd8f04e987a525f03ce0a4c0271e3ec
sjpark <sjpark@bilabhealthcare.com>
2022-01-30 15:36:23 +0900

    Create a Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            title: Text('Ask me anything'),
+          ),
+          body: Container(
+            color: Colors.blue,
+
+          ),
+        ),
       ),
     );
+
+
